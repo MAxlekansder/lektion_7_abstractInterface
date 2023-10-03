99663bfd68ad5af174b564fbf6bb197ad2a19d22
MAxlekansder <144068201+MAxlekansder@users.noreply.github.com>
2023-10-03 09:44:48 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    public interface Animal {
+        void eat();
+        void makeSound();
+        int age(); // om man vill retunera det som en int
+    }
+    
+}
